090edce8f2c0b05a1e0b5ed8d2e3eaa3fd14ad3f
Vinícius Bauermann Almeida <vinib1903@unesc.net>
2024-04-22 21:26:12 -0300

    cria consultas propostas no desafio de projeto

--- Script Filmes.sql
+++ Script Filmes.sql
@@ -1,3 +1,74 @@
+-- Buscar o nome e ano dos filmes
+
+SELECT Nome, Ano FROM Filmes
+
+-- Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano
+
+SELECT Nome, Ano FROM Filmes 
+ORDER BY Ano
+
+-- Buscar pelo filme de volta para o futuro, trazendo o nome, ano e a duração
+
+SELECT Nome, Ano, Duracao FROM Filmes
+WHERE Nome = 'De volta para o futuro'
+
+-- Buscar os filmes lançados em 1997
+
+SELECT * FROM Filmes
+WHERE Ano = 1997
+
+-- Buscar os filmes lançados APÓS o ano 2000
+
+SELECT * FROM Filmes
+WHERE Ano > 2000
+
+-- Buscar os filmes com a duracao maior que 100 e menor que 150, ordenando pela duracao em ordem crescente
+
+SELECT * FROM Filmes
+WHERE 100 < Duracao AND Duracao < 150
+ORDER BY Duracao
+
+-- Buscar a quantidade de filmes lançadas no ano, agrupando por ano, ordenando pela quantidade em ordem decrescente
+
+SELECT Ano, COUNT(Ano) AS Quantidade FROM Filmes
+GROUP BY Ano
+ORDER BY Quantidade DESC
+
+-- Buscar os Atores do gênero masculino, retornando o PrimeiroNome, UltimoNome
+
+SELECT PrimeiroNome, UltimoNome FROM Atores
+WHERE Genero = 'M'
+
+-- Buscar os Atores do gênero feminino, retornando o PrimeiroNome, UltimoNome, e ordenando pelo PrimeiroNome
+
+SELECT PrimeiroNome, UltimoNome FROM Atores
+WHERE Genero = 'F'
+ORDER BY PrimeiroNome
+
+-- Buscar o nome do filme e o gênero
+
+SELECT F.Nome AS NomeFilme, G.Genero
+FROM Filmes F
+INNER JOIN FilmesGenero FG ON F.Id = FG.IdFilme
+INNER JOIN Generos G ON FG.IdGenero = G.Id
+
+-- Buscar o nome do filme e o gênero do tipo "Mistério"
+
+SELECT F.Nome AS NomeFilme, G.Genero
+FROM Filmes F
+INNER JOIN FilmesGenero FG ON F.Id = FG.IdFilme
+INNER JOIN Generos G ON FG.IdGenero = G.Id
+WHERE G.Genero = 'Mistério'
+
+-- Buscar o nome do filme e os atores, trazendo o PrimeiroNome, UltimoNome e seu Papel
+
+SELECT F.Nome AS NomeFilme, A.PrimeiroNome, A.UltimoNome, EF.Papel
+FROM Filmes F
+INNER JOIN ElencoFilme EF ON F.Id = EF.IdFilme
+INNER JOIN Atores A ON EF.IdAtor = A.Id
+
+
+
 CREATE DATABASE [Filmes]
 GO
 USE [Filmes]
